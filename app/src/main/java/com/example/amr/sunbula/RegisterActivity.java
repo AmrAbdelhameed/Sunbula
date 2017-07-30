54e409ee5d419eeafe21b6662a4027da6986bf12
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-07-30 15:25:57 +0200

    7atet el select image from camera and gallery

--- app/src/main/java/com/example/amr/sunbula/RegisterActivity.java
+++ app/src/main/java/com/example/amr/sunbula/RegisterActivity.java
@@ -1,30 +1,88 @@
 package com.example.amr.sunbula;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class RegisterActivity extends AppCompatActivity {
 
     ImageView user_profile;
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private String userChoosenTask;
     EditText username, password, email;
-    Button btn_register, btn_login_facebok;
+    Button btn_register;
+    LoginButton btn_login_facebok;
+    CallbackManager c;
+    Bitmap bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.amr.sunbula",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d("", "");
+
+        } catch (NoSuchAlgorithmException e) {
+            Log.d("", "");
+        }
+
         user_profile = (ImageView) findViewById(R.id.imageregister);
+
+        user_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
         username = (EditText) findViewById(R.id.txtusernameregister);
         password = (EditText) findViewById(R.id.txtpasswordregister);
         email = (EditText) findViewById(R.id.txtemailregister);
         btn_register = (Button) findViewById(R.id.btn_register);
-        btn_login_facebok = (Button) findViewById(R.id.btn_login_facebok);
+        btn_login_facebok = (LoginButton) findViewById(R.id.btn_login_facebok);
+        c = CallbackManager.Factory.create();
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,12 +103,142 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-        btn_login_facebok.setOnClickListener(new View.OnClickListener() {
+        btn_login_facebok.setReadPermissions("user_friends");
+        btn_login_facebok.setReadPermissions("public_profile");
+        btn_login_facebok.setReadPermissions("email");
+        btn_login_facebok.setReadPermissions("user_birthday");
+
+        LoginManager.getInstance().registerCallback(c,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        AccessToken accessToken = loginResult.getAccessToken();
+                        Profile profile = Profile.getCurrentProfile();
+
+                        username.setText(profile.getId());
+                        email.setText(profile.getName());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        LoginManager.getInstance().logOut();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        username.setText(exception.getMessage());
+                    }
+                });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (userChoosenTask.equals("From Camera"))
+                        cameraIntent();
+                    else if (userChoosenTask.equals("From Library"))
+                        galleryIntent();
+                } else {
+                    //code for deny
+                }
+                break;
+        }
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = {"From Camera", "From Library",
+                "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+        builder.setTitle("Add Photo");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(RegisterActivity.this, "Facebook", Toast.LENGTH_SHORT).show();
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(RegisterActivity.this);
+
+                if (items[item].equals("From Camera")) {
+                    userChoosenTask = "From Camera";
+                    if (result) {
+                        cameraIntent();
+                    }
+                } else if (items[item].equals("From Library")) {
+                    userChoosenTask = "From Library";
+                    if (result) {
+                        galleryIntent();
+                    }
+
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
             }
         });
+        builder.show();
+    }
 
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        c.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        bitmap = thumbnail;
+        user_profile.setImageBitmap(thumbnail);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        bitmap = bm;
+        user_profile.setImageBitmap(bm);
     }
 }
